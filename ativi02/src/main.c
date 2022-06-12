5ec00a446e6a5389da5bb7e167dcc74bd3a2d694
willyfernandes411 <57838917+willyfernandes411@users.noreply.github.com>
2022-06-12 20:32:34 -0300

    Registradores necessários inicializados

--- ativi02/src/main.c
+++ ativi02/src/main.c
@@ -18,6 +18,32 @@
 #define STM32_GPIOC_ODR          (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOC_BSRR         (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
 
+#define RCC_AHB1ENR_GPIOCEN      (1 << 2)  /* Bit 2:  IO port C clock enable */
+
+#define GPIO_MODER_INPUT           (0) /* Input */
+#define GPIO_MODER_OUTPUT          (1) /* General purpose output mode */
+#define GPIO_MODER_ALT             (2) /* Alternate mode */
+#define GPIO_MODER_ANALOG          (3) /* Analog mode */
+
+#define GPIO_MODER_SHIFT(n)        (n << 1)
+#define GPIO_MODER_MASK(n)         (3 << GPIO_MODER_SHIFT(n))
+
+#define GPIO_OTYPER_PP             (0) /* 0=Output push-pull */
+#define GPIO_OTYPER_OD             (1) /* 1=Output open-drain */
+
+#define GPIO_OT_SHIFT(n)           (n)
+#define GPIO_OT_MASK(n)            (1 << GPIO_OT_SHIFT(n))
+
+#define GPIO_PUPDR_NONE            (0) /* No pull-up, pull-down */
+#define GPIO_PUPDR_PULLUP          (1) /* Pull-up */
+#define GPIO_PUPDR_PULLDOWN        (2) /* Pull-down */
+
+#define GPIO_PUPDR_SHIFT(n)        (n << 1)
+#define GPIO_PUPDR_MASK(n)         (3 << (n << 1))
+
+#define GPIO_BSRR_SET(n)           (1 << (n))
+#define GPIO_BSRR_RESET(n)         (1 << ((n) + 16))
+
 
 
 int main(int argc, char *argv[])
